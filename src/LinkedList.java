226ff2c58d25bfa96f4eb2e3735175d325546f0c
MOHD_ASAD <soff90455@gmail.com>
2019-10-07 21:18:29 +0530

    init commit, Implementation of the Linked List and add the Insert method to it

---
+++ src/LinkedList.java
@@ -0,0 +1,34 @@
+import org.w3c.dom.Node;
+
+import java.util.Scanner;
+import java.io.*;
+public class LinkedList {
+    Node head;//head of linked list.
+    static class Node{
+        int data;
+        Node next;
+        //Constructor.
+        Node(int d) {
+            data = d;
+            next = null;
+        }
+
+        //Method to insert a new node.
+        public static LinkedList insert(LinkedList list, int data) {
+            Node new_node = new Node(data);
+            new_node.next = null;
+            if (list.head == null) {
+                list.head = new_node;
+            } else {
+                Node last = list.head;
+                while (last.next != null) {
+                    last = last.next;
+                }
+                //insert new node at the last node.
+                last.next = new_node;
+            }
+            //return list.
+            return list;
+        }
+    }
+}
