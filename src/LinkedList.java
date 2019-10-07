dc4e4b6aef5a17c6193d670d26836afe5d129ab5
MOHD_ASAD <soff90455@gmail.com>
2019-10-07 21:34:45 +0530

    Create the Print() method to print the linked list.

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -30,9 +30,33 @@ public class LinkedList {
             //return list.
             return list;
         }
-    }
+        //Method to print the Linked list.
+        public static void PrintList(LinkedList list) {
+            Node currNode = list.head;
+            System.out.print("Linked List: ");
+            //Traverse through a linked list.
+            while (currNode != null) {
+                System.out.print(currNode.data + " ");
+                //go to the next node.
+                currNode = currNode.next;
+            }
+        }
 
-    public static void main(String[] args) {
-
+        public static void main(String[] args) {
+            LinkedList linkedList = new LinkedList();
+            //insert the values in the list.
+            linkedList = insert(linkedList, 1);
+            linkedList = insert(linkedList, 2);
+            linkedList = insert(linkedList, 3);
+            linkedList = insert(linkedList, 4);
+            linkedList = insert(linkedList, 5);
+            linkedList = insert(linkedList, 6);
+            linkedList = insert(linkedList, 7);
+            linkedList = insert(linkedList, 8);
+            linkedList = insert(linkedList, 9);
+            linkedList = insert(linkedList, 10);
+            //call the print() method to print the list.
+            PrintList(linkedList);
+        }
     }
 }
